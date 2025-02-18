2e77517f52c64efd6886fca317ce3ed63e74daf5
Netero <m_benab@live.concordia.ca>
2025-02-18 01:27:19 +0100

    solved containsDuplicate problem

--- containsDuplicates/containsDuplicateBruteForce.py
+++ containsDuplicates/containsDuplicateBruteForce.py
@@ -1,3 +1,18 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+"""
 
 def containsDuplicate( nums: list[int]) -> bool:
     for i in range(len(nums)):
