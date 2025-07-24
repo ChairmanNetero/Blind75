55db56c1e2701b374122746808552aa97145d170
Netero <m_benab@live.concordia.ca>
2025-07-23 23:53:55 -0400

    Solved Best Time to Sell problem

---
+++ BestTimeToSell/bestTimeToSell.py
@@ -0,0 +1,46 @@
+"""
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example 1:
+
+Input: prices = [10,1,5,6,7,1]
+
+Output: 6
+Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+Example 2:
+
+Input: prices = [10,8,7,5,2]
+
+Output: 0
+Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+Constraints:
+
+1 <= prices.length <= 100
+0 <= prices[i] <= 100
+
+"""
+
+
+def maxProfit(prices: list[int]) -> int:
+    l = 0
+    max_profit = 0
+
+    for r in range(1, len(prices)):
+        profit = prices[r] - prices[l]
+        if profit > 0:
+            max_profit = max(profit, max_profit)
+
+        else:
+            l = r
+
+    return max_profit
+
+# Difficulty: Easy
+# Method: By using sliding window algorithm
+# Time complexity: O(n)
