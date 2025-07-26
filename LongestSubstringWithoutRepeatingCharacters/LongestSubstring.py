d2f3d4dddb531fd1464584a3b85c43a5fa75a5ec
Netero <m_benab@live.concordia.ca>
2025-07-25 23:18:39 -0400

    Solved Longest Substring Without Repeating Characters problem

---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstring.py
@@ -0,0 +1,41 @@
+"""
+Given a string s, find the length of the longest substring without duplicate characters.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: s = "zxyzxyz"
+
+Output: 3
+Explanation: The string "xyz" is the longest without duplicate characters.
+
+Example 2:
+
+Input: s = "xxxx"
+
+Output: 1
+Constraints:
+
+0 <= s.length <= 1000
+s may consist of printable ASCII characters.
+"""
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    charSet = set()
+    l = 0
+    res = 0
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+
+    return res
+
+# Difficulty: Medium
+# Method: Using Sliding Window algorithm
+# Time complexity: O(n)
